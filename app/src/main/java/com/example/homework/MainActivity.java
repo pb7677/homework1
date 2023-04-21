d193614adf5745e72b26b4de8a7e40c0e9b5c428
pb7677 <pb7677@bs.amalnet.k12.il>
2023-04-21 17:58:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/homework/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.homework;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
+    }
+
+    public void btn(View view) {
+        btn.setText("Oh, yea, I’ve been clicked!");
+    }
+}
